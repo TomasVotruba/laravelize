42da998a973bf8784d5a5d887ffb46e4c45c5f2b
Tomas Votruba <tomas.vot@gmail.com>
2023-03-28 10:14:10 +0200

    misc

--- rector.php
+++ rector.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
+use Rector\Php55\Rector\String_\StringClassNameToClassConstantRector;
 use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
@@ -15,10 +17,17 @@ return static function (RectorConfig $rectorConfig): void {
     ]);
 
     $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_82,
-        \Rector\Set\ValueObject\SetList::TYPE_DECLARATION,
-        \Rector\Set\ValueObject\SetList::CODE_QUALITY,
-        \Rector\Set\ValueObject\SetList::DEAD_CODE,
-        \Rector\Set\ValueObject\SetList::CODING_STYLE,
+        LevelSetList::UP_TO_PHP_81,
+        SetList::TYPE_DECLARATION,
+        SetList::CODE_QUALITY,
+        SetList::DEAD_CODE,
+        SetList::CODING_STYLE,
+    ]);
+
+    $rectorConfig->ruleWithConfiguration(StringClassNameToClassConstantRector::class, [
+        'Illuminate\*',
+        'Symfony\Component\Routing\Annotation\*',
+        'Symfony\Bundle\FrameworkBundle\Controller\AbstractController',
+        'Symfony\Component\Console\Command\Command',
     ]);
 };
